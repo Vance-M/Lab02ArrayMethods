9dfa8bb95d8fd33e858ee295f49d9a5a1c59b241
Vance-M <myveryvills@yahoo.com>
2021-03-23 20:23:51 -0700

    added skip for reduce method

--- reduce.test.js
+++ reduce.test.js
@@ -4,13 +4,15 @@ describe('reduceMethod function', () => {
     it('iterates through each item in an array and performs a reduce callback on it', () => {
 
         const addTwo = (item, sum) => {
-            sum += item;
+            if(item){
+                sum += item;
+            } else sum += 0;
             return sum;
         }
 
         const initialValue = 0;
 
-        const numbers = [1, 2, 3, 4, 5];
+        const numbers = [1, 2, 3, 4, , 5];
         const newArr = reduceMethod(numbers, addTwo);
   
         expect(newArr).toEqual(15);
